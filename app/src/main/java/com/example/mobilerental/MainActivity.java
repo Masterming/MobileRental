3f48d2bcdc130569bcb664655f24283847827957
Kilian Binder <kilian.binder@siemens.com>
2020-12-17 17:06:48 +0100

    GUI Car Selection geupdated
    Neue Activity hinzugefügt (Bestellung)
    
    Nav Graph geupdated

--- app/src/main/java/com/example/mobilerental/MainActivity.java
+++ app/src/main/java/com/example/mobilerental/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
 
+        Button login = findViewById(R.id.button6);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ScrollingActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
