520cb8da7ed6d5a65b579a3a24f5c5c5a901c0c1
Kilian Binder <kilian.binder@siemens.com>
2020-12-17 17:20:50 +0100

    Paar neue Seiten/Activities

---
+++ app/src/main/java/com/example/mobilerental/Verwaltung.java
@@ -0,0 +1,14 @@
+package com.example.mobilerental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Verwaltung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verwaltung);
+    }
+}
